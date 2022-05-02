dcc9242bff35b952e121ee76ed5ab7df45ca30ff
晨晖 田 <t0410ch@gmail.com>
2022-05-02 17:40:49 +0800

    trul final

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -12,39 +12,18 @@ namespace PortScanner
 
     class Program
     {
-        
-
-
          static void Main(string[] args)
         {
             Scan scan = new Scan();
             try
             {
-            
-                if (true) // 端口合理
-                {
-                    //让输入的textbox只读，无法改变
-                    
-                    //创建线程，并创建ThreadStart委托对象
                     Thread process = new Thread(new ThreadStart(scan.PortScan));
-                    process.Start();
-                    //设置进度条的范围
-                    
-
-                    //显示框显示
-                   
-                }
-                else
-                {
-                    //若端口号不合理，弹窗报错
-                   // MessageBox.Show("输入错误，端口范围为[0-65536]!");
-                }
+                    process.Start();    
 
             }
             catch
             {
-                //若输入的端口号为非整型，则弹窗报错
-                //MessageBox.Show("输入错误，端口范围为[0-65536]!");
+
             }
         }
     }
